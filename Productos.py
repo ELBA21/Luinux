da25e2a53c05c9c98de1774ec55b2b8e4621a2a0
myxomat0sis <leonardojofre2003@gmail.com>
2024-04-05 20:52:59 -0300

    Getters y Setters de la clase Producto

--- Productos.py
+++ Productos.py
@@ -3,4 +3,23 @@ class Productos:
         self.nombre = nombre
         self.id = id
         self.precio = precio
-        
+    
+    #Getters
+    def __get_nombre__(self):
+        return self.nombre
+
+    def __get_id__(self):
+        return self.id
+
+    def __get_precio__(self):
+        return self.precio
+    
+    #Setters
+    def __set_nombre__(self, nombre):
+        self.nombre = nombre
+
+    def __set_id__(self, id):
+        self.id = id
+
+    def __set_precio__(self, precio):
+        self.precio = precio     
