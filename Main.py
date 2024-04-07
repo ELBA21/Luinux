285b6b0122590349f974375fbed02dbd7435f707
myxomat0sis <leonardojofre2003@gmail.com>
2024-04-07 00:46:51 -0400

    Pruebas en la ventana

--- Main.py
+++ Main.py
@@ -2,6 +2,20 @@
 #Librería para la interfaz
 from tkinter import * 
 
+def myFunction():
+    print("Agustín pollo")
+
 root = Tk() 
 
-root.mainloop()
+root.title("Inventario") 
+root.iconbitmap("0_Ico.ico")
+root.geometry("720x480") #Tamaño estándar de la ventana
+
+lbl = Label(root, text="Hola :D") #Las siguientes dos líneas mostrar texto
+lbl.pack()
+
+btn = Button(root, text=":0", command= myFunction) #Botón
+btn.config(bg="black", fg="red") #Dar estilo (también se puede hacer desde Button())
+btn.pack()
+
+root.mainloop() #Comienza el loop
