c6e8fe17feb254249a02d12510e85490218bbe18
Nurodayo <cianuroicecream@gmail.com>
2024-04-07 14:07:23 -0400

    primera pantalla

--- Main.py
+++ Main.py
@@ -2,20 +2,45 @@
 #Librería para la interfaz
 from tkinter import * 
 
-def myFunction():
-    print("Agustín pollo")
+def myFunction(): # usen snake_case po pollos
+    print("Leo pollo")
+    print("usuario: " + get_username())
+    print("contra super secreta: " + get_password())
+
+def get_username():
+    return usuario_textbox.get()
+
+def get_password():
+    return password_textbox.get()
+
+def registrarse_action():
+    print("benja weno pal ñacson te tiro to'o el pixi")
+
 
 root = Tk() 
 
 root.title("Inventario") 
-root.iconbitmap("0_Ico.ico")
-root.geometry("720x480") #Tamaño estándar de la ventana
+#root.iconbitmap("0_Ico.ico") #no me esta funcando
+root.geometry("720x480") #Tamaño estándar de la ventana 
 
-lbl = Label(root, text="Hola :D") #Las siguientes dos líneas mostrar texto
-lbl.pack()
+lbl_usuario = Label(root, text="Usuario") #Las siguientes dos líneas mostrar texto
+lbl_usuario.pack()
 
-btn = Button(root, text=":0", command= myFunction) #Botón
-btn.config(bg="black", fg="red") #Dar estilo (también se puede hacer desde Button())
+usuario_textbox = Entry(root, width=24) #el entry es como una caja de texto sin salto de linea
+usuario_textbox.pack()
+
+lbl_password = Label(root, text="Contraseña")
+lbl_password.pack()
+
+password_textbox = Entry(root, width=24, show="●") #el show= es para mostrar otro caracter en vez de un caracter real
+password_textbox.pack()
+
+btn = Button(root, text="Iniciar sesion", command= myFunction) #Botón
+btn.config(bg="white", fg="black") #Dar estilo (también se puede hacer desde Button())
 btn.pack()
+
+registrarse = Button(root, text="Registrarse", command= registrarse_action)
+registrarse.config(bg="white", fg="black")
+registrarse.pack(side=RIGHT)
 
 root.mainloop() #Comienza el loop
