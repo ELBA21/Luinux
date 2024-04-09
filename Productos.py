a6bdbc12cf70f521b2d0c179abdc6c2287c30ac8
Nurodayo <cianuroicecream@gmail.com>
2024-04-09 15:13:38 -0400

    metodos de producto

--- Productos.py
+++ Productos.py
@@ -1,9 +1,12 @@
+import random
+import string
+
 class Productos:
-    def __init__(self, nombre, id, precio, cantidad):
+    def __init__(self, nombre, id, precio, stock):
         self.nombre = nombre
         self.id = id
         self.precio = precio
-        self.cantidad = cantidad
+        self.stock = stock
     
     #Getters
     def get_nombre(self):
@@ -15,8 +18,8 @@ class Productos:
     def get_precio(self):
         return self.precio
     
-    def get_cantidad(self):
-        return self.get_cantidad
+    def get_stock(self):
+        return self.stock
     
     #Setters
     def set_nombre(self, nombre):
@@ -27,5 +30,16 @@ class Productos:
 
     def set_precio(self, precio):
         self.precio = precio     
-    def set_cantidad(self, cantidad):
-        self.get_cantidad = cantidad
+
+    def set_stock(self, stock):
+        self.stock = stock
+
+    def set_autoid(self): #crea una id "unica" para cada objeto basado en su precio y nombre (4 ints para el nombre y dos chars para el precio)
+        random.seed(self.nombre)
+        a = random.randint(1000, 9999)
+        random.seed(self.precio)
+        b = ''.join(random.choices(string.ascii_uppercase, k=2))
+        self.id = str(a) + "-" + b
+
+    def actualizar_stock(self, stock_update): #actualiza sumando y restando al stock existente
+        self.stock = self.stock + stock_update
