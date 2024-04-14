6b3b36017ddab01aba45394d53a119bb4cc6390c
Elba <benjaminalberto.martinez@alumnos.ulagos.cl>
2024-04-14 17:29:07 -0400

    Se agregan pruebas para lista dinamica de productos

--- paginaProductos.py
+++ paginaProductos.py
@@ -1,63 +1,91 @@
 from tkinter import *
-class testeoProducto:
-    def __init__(self, nombre, id, precio, stock):
-        self.nombre = nombre
-        self.id = id
-        self.precio = precio
-        self.stock = stock
-def cargarProductos():
-        arrayDeTesteo = [
-            testeoProducto("Ramen: Rojo","01",790,4),
-            testeoProducto("Ramen: Naranja","02",790,2),
-            testeoProducto("Ramen: Verde","03",790,1),
-            testeoProducto("Ramen: Negro","04",790,0),
-            testeoProducto("Redbull","51",2000,2),
-            testeoProducto("Pepsi","52",1700,4),
-            testeoProducto("Kem Extreme","531",1700,1),
-            testeoProducto("Kem","53",1500,3)
-        ]
-        return arrayDeTesteo
+from Productos import Productos
+from Sucursal import Sucursal
+##Cosas temporales
+surcursal_provisoria = Sucursal(1)
+surcursal_provisoria.productos = []
+print("Cuantos productos quieres agregar?")
+cant = int(input())
+for i in range(cant):
+       testnombre=input()
+       testprecio=int(input())
+       testcant=int(input())
+       surcursal_provisoria.agregar_productos(testnombre,testprecio,testcant)
+
+
+
+
+def testeoDeBotones():
+       print("HOLA ESTOY SIENDO TESTEADO :3 NYA")
+
     
 #===================== Crear la ventana principal
 ventana = Tk()
 ventana.title("Lista de Productos")
 #====================================
 
-#=======================Elementos extra
+#=======================Malla para organizar
 mallaPrincipal = Frame(ventana, bd=2,relief="ridge")
-mallaPrincipal.pack(padx=1, pady=1)
-lista = Listbox(mallaPrincipal, width=50, height=10)
-lista.pack(padx=10, pady=10)
-LabelInformacion = Label(mallaPrincipal,text="Informacion del Producto")
-LabelInformacion.pack(padx=5)
+mallaPrincipal.grid(row=0,column=0)
+#=============================================================
 
-labelNombre = Label(mallaPrincipal, text="Nombre del Producto:")
-labelNombre.pack(anchor=W)
-labelId = Label(mallaPrincipal, text="Id Del Producto")
-labelId.pack(anchor=W)
-labelPrecio = Label(mallaPrincipal, text="Precio del Producto")
-labelPrecio.pack(anchor=W)
-labelCantidad = Label(mallaPrincipal, text="Cantidad del Producto")
-labelCantidad.pack(anchor=W)
+
+                #==========================Elementos de la malla
+                #=======================================
+
+#================ListBox=====
+listBoxProductos = Listbox(mallaPrincipal, width=50, height=10)
+listBoxProductos.grid(row=0, column=0)
+#==========================
+
+
+
+#==============================Botones
+mallaDeBotones = Frame(mallaPrincipal, bd=2, relief="ridge")
+mallaDeBotones.grid(row=1, column=0)
+botonAgregar = Button(mallaDeBotones, text="Agregar")
+botonAgregar.grid(row=0,column=0)
+botonEditar = Button(mallaDeBotones, text="Editar")
+botonEditar.grid(row=0, column=1)
+BotonEliminar = Button(mallaDeBotones, text="Eliminar")
+BotonEliminar.grid(row=0, column=2)
+#=======================================
+
+#===================Labels De Informacion
+mallaDeLabels = Frame(mallaPrincipal, bd=2, relief="ridge")
+mallaDeLabels.grid(row=2, column=0)
+
+LabelInformacion = Label(mallaDeLabels,text="Informacion del Producto")
+LabelInformacion.grid(row=5, column=0)
+labelNombre = Label(mallaDeLabels, text="Nombre del Producto:")
+labelNombre.grid(row=6, column=0, padx=100)
+labelId = Label(mallaDeLabels, text="Id Del Producto")
+labelId.grid(row=7, column=0)
+labelPrecio = Label(mallaDeLabels, text="Precio del Producto")
+labelPrecio.grid(row=8, column=0)
+labelCantidad = Label(mallaDeLabels, text="Cantidad del Producto")
+labelCantidad.grid(row=9, column=0)
 
 #============================================
 
-listaDeProductos = cargarProductos()
+
+
 #=============================================
+
 def SelecProducto(event):
-        i =lista.curselection()[0]
-        producto = listaDeProductos[i]
+        i =listBoxProductos.curselection()[0]
+        producto = surcursal_provisoria.productos[i]
         print(f"Nombre {producto.nombre}, Id: {producto.id} Precio: {producto.precio}, Stock: {producto.stock} ")
-        labelNombre.config(text=f"Nombre: {producto.nombre}")
-        labelId.config(text=f"Nombre: {producto.id}")
-        labelPrecio.config(text=f"Nombre: {producto.precio}")
-        labelCantidad.config(text=f"Nombre: {producto.stock}")
+        labelNombre.config(text=f"Nombre del Producto: {producto.nombre}")
+        labelId.config(text=f"Id del Producto: {producto.id}")
+        labelPrecio.config(text=f"Precio del Producto: {producto.precio}")
+        labelCantidad.config(text=f"Stock del Producto: {producto.stock}")
 
 #===========================================
-for producto in listaDeProductos:
-      lista.insert(END, producto.nombre)
+for producto in surcursal_provisoria.productos:
+      listBoxProductos.insert(END, producto.nombre)
 
-lista.bind("<<ListboxSelect>>", SelecProducto)
+listBoxProductos.bind("<<ListboxSelect>>", SelecProducto)
 
  
 ventana.mainloop()
