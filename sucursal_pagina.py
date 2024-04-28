64da707d607f3c36f8b8f00b1a5ead822a22d45b
myxomat0sis <leonardojofre2003@gmail.com>
2024-04-28 14:05:33 -0400

    Cambios menores (optimización del código anterior)

--- sucursal_pagina.py
+++ sucursal_pagina.py
@@ -18,16 +18,11 @@ def agregar():
     frame3_agregar = Frame(top1)
     frame3_agregar.pack()
 
-    label1_agregar = Label(frame1_agregar, text= "Agregar sucursal", font="Helvetica 15")
-    label1_agregar.pack()
+    label1_agregar = Label(frame1_agregar, text= "Agregar sucursal", font="Helvetica 15").pack()
+    label2_agregar = Label(frame2_agregar, text= "Nombre").grid(row=0, column=0, pady=10)
+    nombre_sucursal = Entry(frame2_agregar).grid(row=0, column=1, pady=10)
 
-    label2_agregar = Label(frame2_agregar, text= "Nombre")
-    label2_agregar.grid(row=0, column=0, pady=10)
-    nombre_sucursal = Entry(frame2_agregar)
-    nombre_sucursal.grid(row=0, column=1, pady=10)
-
-    btn_agregar = Button(frame3_agregar, text="Crear", bg= "lightgrey")
-    btn_agregar.pack()
+    btn_agregar = Button(frame3_agregar, text="Crear", bg= "lightgrey").pack()
 
 
 # Función de modificar (en proceso)
@@ -37,8 +32,7 @@ def modificar():
     top2.title("Modificar sucursal")
     frame1_modificar = Frame(top2)
     frame1_modificar.pack(pady=10)
-    label_modificar = Label(frame1_modificar, text= "Modificar sucursal", font="Helvetica 15")
-    label_modificar.pack()
+    label_modificar = Label(frame1_modificar, text= "Modificar sucursal", font="Helvetica 15").pack()
 
 # Función de eliminar (en proceso)
 def eliminar():
@@ -47,8 +41,7 @@ def eliminar():
     top3.title("Eliminar sucursal")
     frame1_eliminar = Frame(top3)
     frame1_eliminar.pack(pady=10)
-    label_eliminar = Label(frame1_eliminar, text= "Eliminar sucursal", font="Helvetica 15")
-    label_eliminar.pack()
+    label_eliminar = Label(frame1_eliminar, text= "Eliminar sucursal", font="Helvetica 15").pack()
 
 # Función de ingresar (en proceso)
 def ingresar():
@@ -69,13 +62,9 @@ label = Label(frame1, text="Sucursales", font="Helvetica 15")
 label.pack(pady=20)
 
 #Botones (después los pongo con for)
-btn1 = Button(frame2, text="Agregar", padx=5, pady=2, command=agregar, bg= "lightgrey")
-btn2 = Button(frame2, text="Modificar", padx=5, pady=2, command=modificar, bg= "lightgrey")
-btn3 = Button(frame2, text="Eliminar", padx=5, pady=2, command=eliminar, bg= "lightgrey")
-
-btn1.grid(row=0, column=0, padx=5)
-btn2.grid(row=0, column=1, padx=5)
-btn3.grid(row=0, column=2, padx=5)
+btn1 = Button(frame2, text="Agregar", padx=5, pady=2, command=agregar, bg= "lightgrey").grid(row=0, column=0, padx=5)
+btn2 = Button(frame2, text="Modificar", padx=5, pady=2, command=modificar, bg= "lightgrey").grid(row=0, column=1, padx=5)
+btn3 = Button(frame2, text="Eliminar", padx=5, pady=2, command=eliminar, bg= "lightgrey").grid(row=0, column=2, padx=5)
 
 #Listbox
 listbox_sucursal = Listbox(frame3, width=40, height=12)
@@ -101,7 +90,6 @@ buscar_sucursal.pack()
 #======================================================
 
 #Último botón
-btn4 = Button(frame5, text="Ingresar", padx=5, command=ingresar, bg= "lightgrey")
-btn4.pack()
+btn4 = Button(frame5, text="Ingresar", padx=5, command=ingresar, bg= "lightgrey").pack()
 
 root.mainloop()
